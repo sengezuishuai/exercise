6734bbdcdc2c498e613a7af305967c635a8e64c0
zhangsenyu <ruanyu66510@163.com>
2022-10-17 21:08:26 +0800

    练习

---
+++ 练习1.1/练习1.1/2022_10_17.c
@@ -0,0 +1,94 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<time.h>
+//int main()
+//{
+//	int num = 1;
+//	int a = 1;
+//	int* abc = &a;
+//	//定义一个指针变量，指针
+//	//名称ptr，就是一个int *类型
+//	//ptr指向一个int类型变量的地址
+//	int* ptr = &num;
+//	//num的地址是多少
+//	//说明一如果要输出一个变量地址，使用格式是%p
+//    //&num表示取出num这个变量的地址
+//	printf("num的值=%d num的地址=%p", num, &num);
+//
+//	//指针语言有地址
+//	printf("\nptr的地址是%p", &ptr);
+//	getchar();
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char ch;
+//	printf("请输入字符："); //1 2 回车
+//	scanf("%c", &ch); // 读取 1
+//	printf("%c", getchar()); //输出 2
+//}
+
+//int main()
+//{
+//	_test();
+//	return 0;
+//}
+//int _test()
+//{
+//	_test2();
+//	return 0;
+//}
+//int _test2()
+//{
+//	printf("hehe");
+//}
+
+////链式访问
+//int main()
+//{
+//	//int len = strlen("abc");
+//	//printf("%d\n", len);
+//	//printf("%d\n", strlen("abc"));//链式访问
+//
+//	//char arr[20]= { 0 };
+//	//char arr2[] = "bit";
+//	//strcpy(arr, arr2);//链式访问
+//	//printf("%s\n", arr);
+//	printf("%d", printf("%d", printf("%d", 43)));//printf()返回的是打印在屏幕上的个数
+//	return 0;
+//}
+
+//#pragma comment(lib,"练习1.1.lib") 
+//#include "实验.h"  //相当与声明
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	//函数声明一下,告诉函数的声明一般放在头文件中
+//	//int Add(int, int);
+//	int c = Add(a, b);
+//	printf("%d\n", c);
+//	return 0;
+//}
+////函数的定义先声明再调用
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+
+//int main()
+//{
+//
+//	return 0;
+//}
+
+  //递归
+int main()
+{
+	
+	return 0;
+}
