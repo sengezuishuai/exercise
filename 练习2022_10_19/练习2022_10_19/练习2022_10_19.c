1930dc701811521c23562b7ad0fd92a4be25d97e
zhangsenyu <ruanyu66510@163.com>
2022-10-19 17:22:04 +0800

    数组练习

--- 练习2022_10_19/练习2022_10_19/练习2022_10_19.c
+++ 练习2022_10_19/练习2022_10_19/练习2022_10_19.c
@@ -2,6 +2,70 @@
 #include<stdio.h>
 #include<string.h>
 #include<math.h>
+#include<time.h>
+
+//数组
+//int main()
+//{
+//	int arr[8];
+//	//char ch[5];
+//	//int aee[常量表达式]
+//	
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "hfh";
+//	char arr2[] = { 'f','f','f' };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d", strlen(arr2));
+//	return 0;
+//}
+
+//[]下标引用操作符
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	arr[4] = 5;
+//	int i = 0;
+//	int sz = sizeof(arr)/sizeof(arr[0]);//sizeof(arr)算的是字节除第一个就是答案
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	/*printf("%x\n", 0X12);
+//	printf("%p\n", 0X12);*/
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		//printf("%p\n", &arr[i]);
+//		printf("&arr[%d]=%p\n", i, &arr[i]);
+//	}
+//	return 0;
+//}
+
+//一维数组是连续存放的；随着数组下标的增长地址是由低到高的
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *p);
+//		p++;
+//	}
+//	return 0;
+//}
+
 int main()
 {
 
