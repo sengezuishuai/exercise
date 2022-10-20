460734f84043beff6b0a0a026773df3e82c2723b
zhangsenyu <ruanyu66510@163.com>
2022-10-20 21:31:07 +0800

    练习

---
+++ 练习2022_10_20/练习2022_10_20/test.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+void menu()
+{
+	printf("");
+}
+
+int main()
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		swich(input)
+		{
+			case 1;
+				printf("三子棋游戏")
+		}
+			
+	} while ();
+		return 0;
+}
