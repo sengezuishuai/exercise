058fad69ba54574a01e1f762d63d94b8a697d953
zhangsenyu <ruanyu66510@163.com>
2022-11-01 13:21:26 +0800

    训练

---
+++ practice_11_01/practice_11_01/practice_11_01.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+#define MAX 5
+
+//int main()
+//{
+//	int i, j;
+//	for (i = 1; i < 10; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%2d ", i, j, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+int main()
+{
+	char arr[10] = {'a','k'};
+	printf("%s", arr);
+	int a=  
+}
